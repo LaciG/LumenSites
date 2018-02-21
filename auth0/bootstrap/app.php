455918985810109711221143fa8b99e98b6ecf16
László Gábor <lacigabi@windowslive.com>
2018-02-21 17:23:17 +0100

    Finished, but auth0 API bugs need to be fixed

--- auth0/bootstrap/app.php
+++ auth0/bootstrap/app.php
@@ -67,6 +67,10 @@ $app->singleton(
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Auth0Middleware::class,
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
